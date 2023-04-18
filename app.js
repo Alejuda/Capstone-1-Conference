85ce8ec14a4e4175f8fa52eeec6f7836ae33c7cd
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-04-18 16:25:16 -0300

    Update app.js
    Created speakers object

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const hamburguer = document.getElementById('nav-btn');
-const navMenu = document.getElementById('mobile-menu');
+const hamburguer = document.getElementById("nav-btn");
+const navMenu = document.getElementById("mobile-menu");
 
-const closeMenu = document.querySelectorAll('.close-mobile-menu');
+const closeMenu = document.querySelectorAll(".close-mobile-menu");
 
+const speakers = [
+  {
+    name: "Dr. CynthiaBreazeal",
+    description: "MIT Media Lab",
+    talkAbout: '\"Designing Social Robots: The Next Generation of Intelligent Machines\"',
+    img: "./assets/img/breazeal.png"
+},
+  {
+    name: "Dr. Fei-Fei Li",
+    description: "Stanford University",
+    talkAbout: '\"Deep Learning for Computer Vision: Challenges and Opportunities\"',
+    img: "./assets/img/fei-fei.png"
+  }
+];
 
-hamburguer.addEventListener('click', function(e) {
-    navMenu.classList.add('show');
-})
+hamburguer.addEventListener("click", function (e) {
+  navMenu.classList.add("show");
+});
 
-Array.from(closeMenu).forEach( (button) => {
-    button.addEventListener('click', function(e) {
-        navMenu.classList.remove('show');
-    });
-}
-);
+Array.from(closeMenu).forEach((button) => {
+  button.addEventListener("click", function (e) {
+    navMenu.classList.remove("show");
+  });
+});
