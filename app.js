e615eceb9cd21354a8ecaae5eb1e7ee5ee444db5
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-04-18 17:50:15 -0300

    Update app.js
    Created render function to render all speakers when 'more' button is pressed

--- app.js
+++ app.js
@@ -1,22 +1,76 @@
 const hamburguer = document.getElementById("nav-btn");
 const navMenu = document.getElementById("mobile-menu");
+const seeMoreBtn = document.getElementById("see-more");
 
 const closeMenu = document.querySelectorAll(".close-mobile-menu");
 
+const speakerContainer = document.getElementById("speakers-container");
+
 const speakers = [
   {
-    name: "Dr. CynthiaBreazeal",
+    name: "Dr. Cynthia Breazeal",
     description: "MIT Media Lab",
-    talkAbout: '\"Designing Social Robots: The Next Generation of Intelligent Machines\"',
-    img: "./assets/img/breazeal.png"
-},
+    talkAbout:
+      "Designing Social Robots: The Next Generation of Intelligent Machines",
+    img: "./assets/img/breazeal.png",
+  },
   {
     name: "Dr. Fei-Fei Li",
     description: "Stanford University",
-    talkAbout: '\"Deep Learning for Computer Vision: Challenges and Opportunities\"',
-    img: "./assets/img/fei-fei.png"
-  }
+    talkAbout:
+      "Deep Learning for Computer Vision: Challenges and Opportunities",
+    img: "./assets/img/fei-fei.png",
+  },
+  {
+    name: "Andrew Ng",
+    description: "Founder of deeplearning.ai",
+    talkAbout: "Advancements in Deep Learning and AI Applications",
+    img: "./assets/img/ng.jpg",
+  },
+  {
+    name: "Yoshua Bengio",
+    description: "University of Montreal",
+    talkAbout: "Recent Developments and Future Directions in Deep Learning",
+    img: "./assets/img/bengio.jpeg",
+  },
+  {
+    name: "Dr. Kai-Fu Lee",
+    description: "Sinovation Ventures",
+    talkAbout: "AI and its Impact on the Future of Work and Society",
+    img: "./assets/img/lee.jpg",
+  },
+  {
+    name: "Dr. Kate Crawford",
+    description: "Microsoft Research",
+    talkAbout: "The Societal Implications of AI and its Ethical Considerations",
+    img: "./assets/img/crawford.jpeg",
+  },
 ];
+
+let speakersInner = '';
+
+const renderSpeakers = function (num) {
+    for (let i=0; i<num; i++) {
+        speakersInner += `
+        <div class="speaker-card">
+            <img class="speaker-img" src="${speakers[i].img}" alt="${speakers[i].name + ' image'}">
+            <div class="speaker-info">
+                <h3 class="speaker-name lato">${speakers[i].name}</h3>
+                <h4 class="speaker-description lato">${speakers[i].description}</h4>
+                <div class="divisor-grey"></div>
+                <p class="speaker-thematic roboto">"${speakers[i].talkAbout}"</p>
+            </div>
+        </div>
+    `;
+    }
+    speakerContainer.innerHTML = speakersInner;
+    seeMoreBtn.classList.add('hide');
+    speakersInner = ''
+    return speakersInner
+}
+
+
+
 
 hamburguer.addEventListener("click", function (e) {
   navMenu.classList.add("show");
@@ -27,3 +81,7 @@ Array.from(closeMenu).forEach((button) => {
     navMenu.classList.remove("show");
   });
 });
+
+
+renderSpeakers(2);
+seeMoreBtn.classList.remove('hide');
