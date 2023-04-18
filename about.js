a29546a49497c1a6d0376b302af36ce3418bbb35
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-04-18 18:46:18 -0300

    Create about.js
    Added logic to burguer button

---
+++ about.js
@@ -0,0 +1,13 @@
+const hamburguer = document.getElementById("nav-btn");
+const navMenu = document.getElementById("mobile-menu");
+const closeMenu = document.querySelectorAll(".close-mobile-menu");
+
+hamburguer.addEventListener("click", function (e) {
+    navMenu.classList.add("show");
+  });
+  
+  Array.from(closeMenu).forEach((button) => {
+    button.addEventListener("click", function (e) {
+      navMenu.classList.remove("show");
+    });
+  });
